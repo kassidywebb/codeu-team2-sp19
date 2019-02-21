2ab794d1666959b4c225cae1fb021a98bb78fa48
Kassidy Webb <KassidyWebb@Stonewalls-Mac-Pro.local>
2019-02-21 15:18:44 -0600

    fixing appengine bug

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -54,32 +54,32 @@ public class Datastore {
    *     message. List is sorted by time descending.
    */
    public List<Message> getMessages(String recipient) {
-     List<Message> messages = new ArrayList<>();
+    List<Message> messages = new ArrayList<>();
 
-     Query query =
-         new Query("Message")
-             .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
-             .addSort("timestamp", SortDirection.DESCENDING);
-     PreparedQuery results = datastore.prepare(query);
+    Query query =
+        new Query("Message")
+            .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
+            .addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
 
-     for (Entity entity : results.asIterable()) {
-       try {
-         String idString = entity.getKey().getName();
-         UUID id = UUID.fromString(idString);
-         String user = (String) entity.getProperty("user");
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
 
-         String text = (String) entity.getProperty("text");
-         long timestamp = (long) entity.getProperty("timestamp");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
 
-         Message message = new Message(id, user, text, timestamp, recipient);
-         messages.add(message);
-       } catch (Exception e) {
-         System.err.println("Error reading message.");
-         System.err.println(entity.toString());
-         e.printStackTrace();
-       }
-     }
+        Message message = new Message(id, user, text, timestamp, recipient);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
 
-     return messages;
-   }
+    return messages;
+  }
 }
