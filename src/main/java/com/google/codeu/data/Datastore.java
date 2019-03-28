254a104a844a88711b12361a474153fc45aa4d38
Ajide49 <a.usman9098@gmail.com>
2019-03-27 21:45:31 -0700

    Added store and load functions the datastore file

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -107,7 +107,7 @@ public class Datastore {
 				 without scores. This sets the old sentiment scores to 0 for old messages
 				 */
 				float sentimentScore = entity.getProperty("sentimentScore") == null? (float) 0.0 : ((Double) entity.getProperty("sentimentScore")).floatValue();
-				
+
 				String imageUrl = (String) entity.getProperty("imageUrl");
 
 				Message message = new Message(id, user, text, timestamp, recipient, sentimentScore, imageUrl);
@@ -194,6 +194,99 @@ public class Datastore {
 		String text = largestText(results);
 	return text;
 	}
+
+	/** Stores the Event in Datastore. */
+	public void storeEvent(Event event) {
+		Entity eventEntity = new Entity("Event", event.getId().toString());
+		eventEntity.setProperty("user", event.getUser());
+		eventEntity.setProperty("text", event.getText());
+		eventEntity.setProperty("timestamp", event.getTimestamp());
+    	eventEntity.setProperty("recipient", event.getRecipient());
+		eventEntity.setProperty("sentimentScore", event.getSentimentScore());
+		if(event.getImageUrl() != null) {
+  			eventEntity.setProperty("imageUrl", event.getImageUrl());
+		}
+		datastore.put(eventEntity);
+	}
+
+	/**
+	 * This method takes in an arraylist and query of all events.
+	 * It then loops through the results query and saves the information
+	 * to a event variable to inserted into the message arraylist.
+	 *
+	 * @param events message arraylist
+	 * @param results the individual events to be parsed
+	 * @param the user/creactor of the event
+	 */
+
+
+	public void saveEventInformation(List<Event> events, PreparedQuery results) {
+
+		for (Entity entity : results.asIterable()) {
+			try {
+				String idString = entity.getKey().getName();
+				UUID id = UUID.fromString(idString);
+				String user = (String) entity.getProperty("user");
+				String text = (String) entity.getProperty("text");
+				long timestamp = (long) entity.getProperty("timestamp");
+				String recipient = (String) entity.getProperty("recipient");
+
+				/*Before adding sentiment scores as a feature, there were already messages
+				 without scores. This sets the old sentiment scores to 0 for old messages
+				 */
+				float sentimentScore = entity.getProperty("sentimentScore") == null? (float) 0.0 : ((Double) entity.getProperty("sentimentScore")).floatValue();
+
+				String imageUrl = (String) entity.getProperty("imageUrl");
+
+				Event event = new Event(id, user, text, timestamp, recipient, sentimentScore, imageUrl);
+				events.add(event);
+			} catch (Exception e) {
+				System.err.println("Error reading message.");
+				System.err.println(entity.toString());
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	/**
+	 * Gets events posted by a specific user.
+	 *
+	 * @return a list of events posted by the user, or empty list if user has never posted a
+	 *     message. List is sorted by time descending. This is now dealt in saveEventInformation()
+	 */
+
+	public List<Event> getEvents(String recipient) {
+		List<Event> events = new ArrayList<>();
+
+		Query query =
+				new Query("Event")
+				.setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
+				.addSort("timestamp", SortDirection.DESCENDING);
+		PreparedQuery results = datastore.prepare(query);
+
+		saveEventInformation(events, results);
+
+		return events;
+	}
+
+	/**
+	 * Similar to the getEvents function, fetches all the events regardless
+	 * of user. Uses helper function saveEventInformation
+	 * of user.
+	 * @return
+	 */
+	public List<Event> getAllEvents(){
+		List<Event> events = new ArrayList<>();
+
+		Query query = new Query("Event")
+				.addSort("timestamp", SortDirection.DESCENDING);
+		PreparedQuery results = datastore.prepare(query);
+
+		saveEventInformation(events, results);
+
+    return events;
+  }
 
 
 }
