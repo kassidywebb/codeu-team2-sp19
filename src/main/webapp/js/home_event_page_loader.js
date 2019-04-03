72c113add6800c18f8577cef0f1c78c0de0f5d1a
Imani Tyson <imanistyson@gmail.com>
2019-04-03 14:57:07 -0500

    Update home_event_page_loader.js

--- src/main/webapp/js/home_event_page_loader.js
+++ src/main/webapp/js/home_event_page_loader.js
@@ -3,9 +3,9 @@
     const url = '/home';
     fetch(url).then((response) => {
       return response.json();
-    }).then((messages) => {
+    }).then((events) => {
       const eventContainer = document.getElementById('event-container');
-      if(messages.length == 0){
+      if(messagventses.length == 0){
        eventContainer.innerHTML = '<p>There are no posts yet.</p>';
       }
       else{
@@ -36,13 +36,13 @@
    headerDiv.appendChild(timeDiv);
 
    const bodyDiv = document.createElement('div');
-   bodyDiv.classList.add('message-body');
+   bodyDiv.classList.add('event-body');
    /* Changed how bodyDiv get's the message text in feed js. It previously
       did not upload pictures to the feed.html using appendChild, so I copied
       how the div was implemented in user-page-loader.js when taking in the
       text using innerHTML
     */
-   bodyDiv.innerHTML = message.text;
+   bodyDiv.innerHTML = event.text;
    //bodyDiv.appendChild(document.createTextNode(message.text));
 
    const eventDiv = document.createElement('div');
