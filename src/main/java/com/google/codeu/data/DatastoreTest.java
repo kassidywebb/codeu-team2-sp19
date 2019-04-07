b33a37d3d1ccd870ff6ab751bd30a4106a9817f8
Ajide49 <a.usman9098@gmail.com>
2019-04-06 21:49:32 -0700

    Changed pom.xml and datastore

--- src/main/java/com/google/codeu/data/DatastoreTest.java
+++ src/main/java/com/google/codeu/data/DatastoreTest.java
@@ -32,33 +32,38 @@ public class DatastoreTest {
 	 
    Datastore datastore = new Datastore();
 	 
-   Event exampleEvent = new Event("Ahmed", "HotPocket Event", "03/29/19",
+   Event exampleEvent = new Event("", "Ahmed", "HotPocket Event", "03/29/19",
    		"3:00PM", 4523, "Sunnyvalue, CA", "It will be fun and have lots of food",
    		"some random img string");
+   
+   Event exampleEvent2 = new Event("","Demha", "Orchestra Maze", "03/31/19",
+		   "8:00PM", 8472523, "SanFrancisco, CA", "It will be fun and have lots of music", "a cello  and violion img url");
+		   	datastore.storeEvent(exampleEvent2);
+		   	
 
    /*Create an event entity using exampleEvent and populate it into datastore by calling     storeEvent*/
      datastore.storeEvent(exampleEvent);
      /* Check if there is 1 event inside the datastore right now*/
-  	assertEquals(1, datastore.numberOfEvents());
+  	//assertEquals(1, datastore.numberOfEvents());
   	//assertEquals(1, ds.prepare(new Query("Event")).countEntities(withLimit(10)));
   	
   	  /* When getEvents is called an arrayList of Events is created according to the 
   	   * user specified in the parameter. In this case getEvents should create a list
   	   * holding 1 object from user Ahmed.
   	   */
- 	assertEquals(1, datastore.getEvents("Ahmed").size());
+     datastore.storeEvent(exampleEvent2);
+ 	assertEquals(1, datastore.getEvents(exampleEvent.getUser()).size());
   	
-	Event exampleEvent2 = new Event("Demha", "Orchestra Maze", "03/31/19",
-"8:00PM", 8472523, "SanFrancisco, CA", "It will be fun and have lots of music", "a cello     and violion img url");
-	datastore.storeEvent(exampleEvent2);
 	
 	/*getAllEvents returns an arraylist of events regardless of User. 
 	 * With the second added event, there should now be two  events in
 	 * the list right now.
 	 */
-  	assertEquals(2, datastore.getAllEvents().size());
+  assertEquals(2, datastore.getAllEvents().size());
   	
   	assertEquals(exampleEvent.getId(), datastore.getEvents("Ahmed").get(0).getId());
+  	
+  	assertEquals(exampleEvent.getId(), datastore.getIndividualEvent(exampleEvent.getId()).getId());
   	
  }
  @Test //where testing function is called
