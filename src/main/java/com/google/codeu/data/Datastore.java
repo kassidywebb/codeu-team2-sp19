5aaa1b8ecf0730664b3d6ce58b4c12b205bfb647
Ahmed Usman <42790748+Ajide49@users.noreply.github.com>
2019-04-10 19:02:13 -0700

    Update Datastore.java

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -17,7 +17,6 @@
 package com.google.codeu.data;
 
 import com.google.appengine.api.datastore.DatastoreService;
-
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
